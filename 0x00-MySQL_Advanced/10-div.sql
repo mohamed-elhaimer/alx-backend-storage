d734b6ef1cfbb9ee2d6ad57094982bd92161236b
Logic <mohamedelheimer0@gmail.com>
2024-05-13 00:01:46 +0100

    all

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,14 @@
+--  creates a function SafeDiv that divides (and returns).
+DELIMITER $$
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURN FLOAT DETERMINISTIC
+BEGIN
+DECLARE result FLOAT;
+IF b = 0
+THEN
+SET result = 0;
+ElSE
+SET result = a / b;
+END IF;
+RETURN result;
+ENd$$
