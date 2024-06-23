2ca0e724ac0f29a216c3672ac4ce7aab485b05ee
Logic <mohamedelheimer0@gmail.com>
2024-06-23 14:46:51 +0100

    all

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ 12-log_stats.py """
 import redis
 import uuid
-from typing import Union
+from typing import Union, callable, Optional
 
 
 class Cache:
@@ -15,3 +15,25 @@ class Cache:
         id = str(uuid.uuid4())
         self._redis.set(id, data)
         return id
+
+    def get(self, key: str, fn: Optional[callable] = None) -> Union[str, bytes, int, float]:
+        """convert the data back to the desired format."""
+        value = self._redis.get(key)
+        if fn and  value:
+            value = fn(value)
+            return value
+        return value
+    def get_str(self, key: str) -> Optional[str]:
+        """get_str"""
+        return self.get(key, lambda x: x.decode("utf-8"))
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieve an integer value from Redis.
+
+        Args:
+            key (str): The key of the integer to retrieve.
+
+        Returns:
+            Optional[int]: The integer value, or None if key does not exist.
+        """
+        return self.get(key, lambda x: int(x))
