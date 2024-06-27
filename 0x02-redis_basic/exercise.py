4fb56172590d4c9a7947ada86d65399adb4d147d
Logic <mohamedelheimer0@gmail.com>
2024-06-27 13:58:54 +0100

    all

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -31,6 +31,29 @@ def call_history(method: Callable) -> Callable:
     return wrapper
 
 
+def replay(method: Callable) -> None:
+    """ replay the history """
+    redis_client = redis.Redis()
+    methodname = method.__qualname__
+    listin = redis_client.lrange(f"{methodname}:inputs", 0, -1)  # nopep8
+    listout = redis_client.lrange(f"{methodname}:outputs", 0, -1)  # nopep8
+
+    in_and_out = zip(listin, listout)
+    in_and_out_list = list(in_and_out)  # Store the result of zip in a list
+    print(f"Cache.store was called {len(in_and_out_list)} times:")
+    for invalue, outvalue in in_and_out_list:
+        # Convert binary strings to regular strings
+        if isinstance(invalue, bytes):
+            invalue_str = f"{invalue.decode('utf-8')}"
+        elif isinstance(invalue, int):
+            invalue_str = f"{invalue}"
+        else:
+            invalue_str = f"{invalue}"
+
+        outvalue_str = outvalue.decode('utf-8')
+        print(f"{methodname}(*{invalue_str}) -> {outvalue_str}")
+
+
 class Cache:
     def __init__(self) -> None:
         self._redis = redis.Redis()
